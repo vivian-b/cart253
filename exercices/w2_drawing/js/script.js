58b7453eb990e2614482a26b33dcc9f2a3d53ddf
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2020-09-16 21:22:04 -0400

    Color Start

--- exercices/w2_drawing/js/script.js
+++ exercices/w2_drawing/js/script.js
@@ -9,7 +9,34 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
-background (250,200,0);
+//r,g,b, transparency: between 0 and 255
+//x,y,w,h
+
+  createCanvas(500,500)
+
+  background (250,200,0);
+
+  point(250,250);
+
+  rect(0,0,250,500);
+  //rectangle are drawn from the top left corner
+// rectMode(center): will apply on all rectangles in the document
+  line(0,0,500,500);
+  line(500,0,0,500);
+  //corner to corner
+
+fill(1,1,1);
+stroke(250,250,250);
+//applies color to anything UNDER vvv
+
+  ellipse(250,250,250,250);
+noStroke();
+//cancels anything under vvv (noFill)
+
+  ellipse(250,50,50,50);
+
+  //ellipses are drawn from the center
+// ellipseMode(corner): will start top left corner
 }
 
 // draw()
