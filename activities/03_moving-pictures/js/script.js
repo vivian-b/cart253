d8b0df7549033be04d94abe408bf2a7d0b07225e
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2020-09-23 23:34:17 -0400

    A3: finish

--- activities/03_moving-pictures/js/script.js
+++ activities/03_moving-pictures/js/script.js
@@ -1,14 +1,41 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Vivian Bui
 
-Here is a description of this template p5 project.
+Moving Objects
 **************************************************/
 
 // setup()
 //
+
+let bg = {
+  r: 0,
+  g: 0,
+  b: 0,
+};
+
+let circle1 = {
+  x: 0,
+  y: 250,
+  size: 180,
+  fill: 255,
+  speed: 1,
+  alpha: 200
+};
+
+let circle2 = {
+  x: 500,
+  y:250,
+  size: 75,
+  fill: 255,
+  speed: -1,
+  alpha: 200
+}
+
+
 // Description of setup() goes here.
 function setup() {
+  createCanvas(500,500);
+  noStroke();
 
 }
 
@@ -16,5 +43,28 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+
+//background
+  background(bg.r,bg.g,bg.b);
+  bg.r = bg.r + 1;
+  bg.r = map(circle1.size, 100,width,0,255);
+
+  //left circle
+  circle1.x += circle1.speed;
+  circle1.x = constrain(circle1.x,0,width/2);
+  circle1.size += 0.25;
+  circle1.size = constrain(circle1.size,0,width);
+
+    fill(circle1.fill,circle1.alpha);
+    ellipse(circle1.x,circle1.y,circle1.size);
+
+  //right circle
+    circle2.x += circle2.speed;
+    circle2.x = constrain(circle2.x,width/2,width);
+    circle2.size = circle1.size * 0.75;
+
+
+      fill(circle2.fill,circle2.alpha);
+      ellipse(circle2.x,circle2.y,circle2.size);
 
 }
