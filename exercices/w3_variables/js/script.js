2cbb0c68c5dd631670d74c502ed82c3ea476c93b
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2020-09-23 21:35:19 -0400

    W3 Variable - Part 3

--- exercices/w3_variables/js/script.js
+++ exercices/w3_variables/js/script.js
@@ -3,14 +3,18 @@ Variable Experiments
 Vivian Bui
 **************************************************/
 
-//variable names: (declares variables, nicknames, references)
+//variable names: (declares/set variables, nicknames, references)
 //element that uses it has the determined number
 // = assignment operator + value
+//variables > numbers (better usage)
 
 let bgShade = 0;
-let circleSize = 300;
+let circleSize = 500;
 let circleX = 250;
 let circleY = 250;
+let circleSpeed = 1;
+backgroundShade = 0;
+let circleAcceleration = 0.25;
 
 // setup()
 //
@@ -24,17 +28,33 @@ createCanvas(windowWidth,windowHeight);
 // Description of draw() goes here.
 function draw() {
 //changing background color depending on mouse location
-
-background(bgShade)
-
 //moving the square with the mouse position
 
 rectMode(CENTER)
-rect(0,mouseY/2,mouseY,mouseX);
+rect(mouseX,mouseY/2,mouseY,mouseX);
 
 //moving the square's dimension depending one the mouse location//
 rectMode(CENTER)
 fill(mouseX,mouseY/2,0)
-circle(circleX,circleY,mouseX/10,mouseY/2);
+
+
+
+//following the variable
+//circle(circleX,circleY,mouseX/10,mouseY/2);
+
+//unfollowing/cancelling the variable
+// variable+maths makes it animate since it loops every fame
+//changes relatively to itself
+backgroundShade = backgroundShade + 2;
+circleSize = circleSize+circleSpeed;
+
+//position being controlled by several X/Y,
+//speed being controlled by velocity (# moves each timestep/frame)
+
+// A = A-B ->> A += -B  (-= subtract the value, /= divide, *= multiply)
+//circleX = circleX - circleAcceleration;
+circleX *= circleAcceleration;
+circle(circleX,circleY,circleSize);
+
 
 }
