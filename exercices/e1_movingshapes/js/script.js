c7eda1d70f56e091d528d6ab634a54a4b298ba61
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2020-09-24 20:57:06 -0400

    E1: Finish

--- exercices/e1_movingshapes/js/script.js
+++ exercices/e1_movingshapes/js/script.js
@@ -9,20 +9,17 @@ and anything else you want to experiment with.
 // setup()
 //
 
-let bg = {
-  r: 0,
-  g: 0,
-  b: 0,
-};
+//bacground
+  let bg = 0
 
 // Left
-let circleA = {
-  x: 100,
-  y: 0,
-  s: 10,
-  fill: 0,
-  speed: 7,
-  alpha: 100
+  let circleA = {
+    x: 100,
+    y: 0,
+    s: 5,
+    fill: 0,
+    speed: 2,
+    alpha: 125
 };
 
 // Center
@@ -31,7 +28,7 @@ let circleB = {
   y: 0,
   s: 20,
   fill: 0,
-  speed: 7,
+  speed: 4,
   alpha: 200
 }
 
@@ -41,57 +38,66 @@ let circleC = {
   y: 0,
   s: 30,
   fill: 0,
-  speed: 7,
+  speed: 8,
   alpha: 300
 }
 
-// Description of setup() goes here.
+// Canvas Size
 function setup() {
   createCanvas(500,500);
   noStroke();
 
 }
 
-// draw()
-//
-// Three circles lined up diagonally in different colors depending on
-// the cursor's location.
+
+// Three circles (#1) dropping down at different speed (#2) and enlarges (#3)
+// lined up diagonally (#5.2) in different colors depending on the cursor(#4 & #6)
+// with a grey background that darkens/lightens depending on the cursor (#5.1)
+
 function draw() {
 
-//background
-background(bg.r,bg.g,bg.b);
-bg.r += 1;
-bg.r = map(circleA.s, 50,width,0,255);
+//background (map)
 
-// circle A
-  circleA.y += circleA.speed;
-  circleA.y = constrain(circleA.y,0,width/4);
+  background(bg)
+  bg = map(mouseX,0,width,100,180)
 
-  circleA.s += .53;
-  circleA.s = constrain(circleA.s,0,width/4);
+// circle A: left
 
-    fill(mouseY/4,mouseX,circleA.alpha);
-    ellipse(circleA.x,circleA.y,circleA.s);
+  //vertical movement
+    circleA.y += circleA.speed;
+    circleA.y = constrain(circleA.y,0,width/4);
+    
+  //size change
+    circleA.s += .53;
+    circleA.s = constrain(circleA.s,0,width/4);
 
-// circle B
+      fill(mouseY/3,mouseX,circleA.alpha);
+      ellipse(circleA.x,circleA.y,circleA.s);
+
+// circle B: center
+
+  //vertical movement
     circleB.y += circleB.speed;
     circleB.y = constrain(circleB.y,0,width/2);
 
+  //size change
     circleB.s += .5;
     circleB.s = constrain(circleB.s,0,width/4);
 
-      fill(mouseY/3,mouseX/3,circleB.alpha);
-      ellipse(circleB.x,circleB.y,circleB.s);
+      fill(mouseY,mouseX,circleB.alpha);
+      ellipse(circleB.x-(mouseX/25),circleB.y-(mouseY/25),circleB.s);
 
-// circle C
+// circle C: right
 
+  //vertical movement
     circleC.y += circleC.speed;
     circleC.y = constrain(circleC.y,0,width*3/4);
 
+  //size change
     circleC.s += .75;
     circleC.s = constrain(circleC.s,0,width/4);
 
-      fill(mouseY,mouseX/4,circleC.alpha);
+      fill(mouseY,mouseX/3,circleC.alpha);
       ellipse(circleC.x,circleC.y,circleC.s);
 
 }
