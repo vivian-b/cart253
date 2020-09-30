b42ccfaeb75698a1ba203460d413ee382b758ed0
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2020-09-30 16:27:38 -0400

    week 4: pt 6

--- exercices/w4_loop/js/script.js
+++ exercices/w4_loop/js/script.js
@@ -15,7 +15,15 @@ let caterpillar ={
 let circle = {
   x:250,
   y:250,
-  size:100
+  size:100,
+  //velocity
+  vx:0,
+  vy:0,
+  //acceleration
+  ax: 0,
+  ay: 0,
+  acceleration: 0.25,
+  maxSpeed: 20
 }
 
 let bg = {
@@ -36,8 +44,35 @@ createCanvas(500,500);
 function draw() {
   background(bg.r,bg.g,bg.b);
   noStroke();
-  fill(100,200,100);
 
+//if mouse is to the left of circle (behind x)
+if (mouseX < circle.x) {
+  circle.vx = -circle.acceleration
+}
+//if mouse moves to the opposite, moves the same way
+else{circle.vx = circle.acceleration}
+
+//if mouse is above the circle
+if (mouseY < circle.y){
+  circle.vy = -circle.acceleration;
+}
+
+else {
+  circle.vy = circle.acceleration
+}
+
+//set velocity via acceleration
+circle.vx += circle.ax;
+circle.vx = constrain(circle.vx,-circle.maxSpeed,circle.maxSpeed);
+circle.vy += circle.ay;
+circle.vy = constrain(circle.vy,-circle.maxSpeed,circle.maxSpeed);
+
+
+
+circle.x = circle.x + circle.vx;
+circle.y = circle.y + circle.vy;
+
+ellipse (circle.x,circle.y,circle.size);
 
 //wildloop
 //keeps doing the statement until it becomes false
@@ -62,32 +97,32 @@ function draw() {
 //forloop
 // similar to wildloop but based on counting (in increments of 1 / ++)
 
-let x = caterpillar.x;
-let numSegments = 10;
-
-//starting fact (0) ; smtg to check for it to keep going, to see if the counter has reached its maximum value ; something that makes the counter change
-//for(let segmentsDrawn = 0; segmentsDrawn < numSegments; segmentsDrawn++){
-
-// things we use to count = i (segmentsDrawn)
-
-//start at 0; count up ; by 1
-for(let i = 0; i < numSegments; i++){
-ellipse (x,caterpillar.y, caterpillar.segmentSize);
-x = x + 40;
-}
-
-ellipse(circle.x,circle.y,circle.size)
+// let x = caterpillar.x;
+// let numSegments = 10;
+//
+// //starting fact (0) ; smtg to check for it to keep going, to see if the counter has reached its maximum value ; something that makes the counter change
+// //for(let segmentsDrawn = 0; segmentsDrawn < numSegments; segmentsDrawn++){
+//
+// // things we use to count = i (segmentsDrawn)
+//
+// //start at 0; count up ; by 1
+// for(let i = 0; i < numSegments; i++){
+// ellipse (x,caterpillar.y, caterpillar.segmentSize);
+// x = x + 40;
+// }
+//
+// ellipse(circle.x,circle.y,circle.size)
 
 //makes the circle move to the location that was mousePressed
-function mousePressed() {
-  circle.x = mouseX;
-  circle.y = mouseY;
-}
+// function mousePressed() {
+//   circle.x = mouseX;
+//   circle.y = mouseY;
+// }
 //makes bg change color randomly after every click
-function mousePressed (){
-  bg.r = random(0,255);
-  bg.g = random(0,255);
-  bg.b = random(0,255);
-}
+// function mousePressed (){
+//   bg.r = random(0,255);
+//   bg.g = random(0,255);
+//   bg.b = random(0,255);
+// }
 
 }
