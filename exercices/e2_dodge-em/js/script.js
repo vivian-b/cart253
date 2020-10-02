6e262f6ec48d95306711cc08a0c5f282795bdf35
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2020-10-01 20:29:22 -0400

    E2: Ver.4
    Comments and small tweaks

--- exercices/e2_dodge-em/js/script.js
+++ exercices/e2_dodge-em/js/script.js
@@ -2,69 +2,66 @@
 Exercise 02: Dodge-Em
 Vivian Bui
 
-Create an interactive program where the the user has to avoid
-certain shapes.
+Create an interactive program where the the avatar has to avoid/catch a target.
 **************************************************/
 
-//loadImage
+//avatar (image)
 let victimImage;
 function preload (){
   img = loadImage ('assets/images/victim.png')
 }
 
 //background
-let bg = {
-  r:1,
-  g:20,
-  b:45,
+  let bg = {
+    r:1,
+    g:20,
+    b:45,
 }
 
-//bubble (target)
-let bubble ={
-  x:0,
-  y:250,
-  size: 70,
-  vx: 0,
-  vy: 0,
-  speed: 18,
-  fill: {
-    r:15,
-    g:170,
-    b:245
+//rain (target)
+  let rain ={
+    x:0,
+    y:250,
+    size: 65,
+    vx: 0,
+    vy: 0,
+    speed: 25,
+    fill: {
+      r:15,
+      g:170,
+      b:245
   }
-
 };
 
-//user (avatar)
-let user ={
-  x:250,
-  y:250,
-  size: 110,
-  vx: 0,
-  vy: 0,
-  speed: 5,
-
+// avatar (hitbox)
+  let avatar ={
+    x:250,
+    y:250,
+    size: 135,
+    vx: 0,
+    vy: 0,
+    speed: 5,
 };
 
-//user (ball)
-let ball ={
-
-  x:0,
-  y:250,
-  size: 20,
-  fill: {
-    r:180,
-    g:2,
-    b:70
-  }}
+//ball (user control)
+  let ball ={
+    x:0,
+    y:250,
+    size: 25,
+    fill: {
+      r:180,
+      g:2,
+      b:70
+  }
+}
 
 // setup()
-// canvas size, bubble's speed and spawn point
+// canvas size, rain's drop speed and spawn point
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  bubble.vy = bubble.speed;
-  bubble.x = random(0,height);
+  rain.vy = rain.speed;
+  rain.x = random(0,height);
 
   noStroke();
   noCursor();
@@ -76,57 +73,66 @@ function draw() {
 //background color (static)
   background(bg.r,bg.g,bg.b)
 
-//bubble's movement
-  bubble.x += bubble.vx
-  bubble.y += bubble.vy
-
-//bubble's spawn reset
-// target comes back to the top once it reaches the bottom
-  if (bubble.y > height+bubble.size/2) {
-      bubble.y = 0;
-      bubble.x = random(0,width);
-  }
+//ball's drawing
+  fill(ball.fill.r,ball.fill.g,ball.fill.b);
+  ellipse(mouseX,mouseY,ball.size);
 
 
-//growth of the bubble whenever it is in contact with the user
-  let d = dist(user.x, user.y, bubble.x,bubble.y);
-    if (d < bubble.size/2 + user.size/2) {
-    bubble.size *= 1.1;
+////// RAIN ////////////
+// Object that falls from top to bottom. When it touches <avatar>, it grows.
+// the program stops when the <rain> expands to a certain size.
+
+  //rain's movement (vertical drop)
+  rain.x += rain.vx
+  rain.y += rain.vy
+
+  //rain's spawn reset (comes back to the top once it reaches the bottom)
+    if (rain.y > height+rain.size/2) {
+        rain.y = 0;
+        rain.x = random(0,width);
 }
 
-//cap on the size of the bubble
-// "game over" when it reaches the max
-if (bubble.size > 3*windowHeight){
-  noLoop();
+  //rain expands whenever it's in contact with the avatar's hitbox
+    let d = dist(avatar.x, avatar.y, rain.x,rain.y);
+      if (d < rain.size/2 + avatar.size/2) {
+        rain.size *= 1.05;
 }
 
-//bubble
-fill(bubble.fill.r, bubble.fill.g, bubble.fill.b);
-ellipse(bubble.x,bubble.y,bubble.size);
-
-
-//user hitbox
-noFill(user)
-ellipse(user.x,user.y,user.size)
-
-//user controls
-// user.x = mouseX
-user.y = windowHeight;
-
-if (mouseX > user.x) {
-  user.vx = user.speed;
-}
-else if (mouseX < user.x) {
-  user.vx = -user.speed;
-
+  //cap on the size of the rain -> game ends when it reaches the max size
+    if (rain.size > 4*windowWidth){
+      noLoop();
+      ball.fill = 0
 }
 
-user.x = user.x + user.vx;
+  //rain's drawing
+    fill(rain.fill.r, rain.fill.g, rain.fill.b);
+    ellipse(rain.x,rain.y,rain.size);
 
-//user avatar
-image(img, user.x-78, user.y-86, 140, 110);
+////// AVATAR ////////////
+// The <avatar> is the image that follows the movement of the <ball> which is controlled by the user.
+// only the <avatar> has a hitbox, meaning that the <ball> has no effect from touching the <rain>
 
-fill(ball.fill.r,ball.fill.g,ball.fill.b);
-ellipse(mouseX,mouseY,ball.size);
+//avatar hitbox
+  noFill(avatar)
+  ellipse(avatar.x,avatar.y,avatar.size)
+
+//avatar's controls
+
+//avatar's movement delay (follows the cursor/ball)
+//*** QUESTION: is there a way to make the image flip so it "faces" the cursor?***
+
+  if (mouseX > avatar.x) {
+    avatar.vx = avatar.speed;
+}
+
+  else if (mouseX < avatar.x) {
+    avatar.vx = -avatar.speed;
+}
+//anchored to the bottom of the viewport
+avatar.y = windowHeight;
+avatar.x = avatar.x + avatar.vx;
+
+//avatar's image
+image(img, avatar.x-78, avatar.y-86, 140, 110);
 
 }
