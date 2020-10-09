314384fad3db77c8520e21ea9c8ad1e34e4deb76
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2020-10-09 02:57:39 -0400

    E3: final tweaks and comments

--- exercices/e3_love, actually/js/script.js
+++ exercices/e3_love, actually/js/script.js
@@ -10,7 +10,9 @@ let user ={
   x: 100,
   y: 300,
   size: 75,
-};
+}
+
+//languages in text(string)
 
 let eng ={
   string:`love`,
@@ -54,6 +56,7 @@ let vnm ={
     y:0
 }
 
+//easter egg
 let ily ={
   string:`ily <3`,
   x:0,
@@ -62,35 +65,39 @@ let ily ={
 
 let state = `title`
 
-
 ///////////
 
+//main functions & states
 
 function setup() {
-createCanvas(windowWidth,windowHeight);
+  createCanvas(windowWidth,windowHeight);
 }
 
 function draw() {
-background(200,100,100);
-noStroke();
-simulation();
-noCursor();
+  push();
+  background(217, 61, 113);
+  noStroke();
+  simulation();
+  noCursor();
+  pop();
 
 function title(){
   push();
   textSize(64);
-  fill(0);
+  fill(255);
   textAlign(CENTER,CENTER);
   text(`LOVE`,width/2, height/2);
   pop();
 }
 
-
+//simulation start
 if (state === `title`) {
-title();
-text(`Across Languages`, windowWidth/2 -50,windowHeight/2 +50);
-    text(`press to enter start`, 5,windowHeight-5)}
+  title();
+  fill(255, 171, 199);
+  text(`Across Languages`, windowWidth/2 -50,windowHeight/2 +50);
+  text(`press to enter or click on a word to start`, windowWidth/2 - 105,windowHeight-30)}
 
+// enter key to start
 if (keyIsDown(13)) {
       state = `stimulation`
     }
@@ -100,12 +107,12 @@ if (state === `simulation`){
 }
 
 function simulation(){
-  display();
-  setupSimulation();
-  checkTranslation()
+  checkTranslation(); //hitbox
+  display(); //user control
+  setupSimulation(); //text
 }
 
-function checkTranslation(){
+function checkTranslation(){ //hitbox of the text
 
 if (mouseIsPressed && dist(mouseX,mouseY,eng.x,eng.y) < 25){
       state = `translateENG`;
@@ -139,6 +146,7 @@ if(mouseIsPressed && dist(mouseX,mouseY,ily.x,ily.y)< 35) {
 pause( )}
   }
 
+// corresponding language pop ups for each word when clicking on the text
 if (state === `translateENG`) {
   translateENG()}
 
@@ -152,7 +160,6 @@ if (state === `translateENG`) {
     text(`love`, width/2 -25, height/2 - 90);
     textSize(20)
   }
-
 
   if (state === `translateFR`) {
     translateFR()}
@@ -169,7 +176,7 @@ if (state === `translateENG`) {
       textSize(20)
     }
 
-    if (state === `translateSPN`) {
+  if (state === `translateSPN`) {
       translateSPN()}
 
       function translateSPN() {
@@ -184,7 +191,7 @@ if (state === `translateENG`) {
         textSize(20)
       }
 
-      if (state === `translateKR`) {
+  if (state === `translateKR`) {
         translateKR()}
 
         function translateKR() {
@@ -199,7 +206,7 @@ if (state === `translateENG`) {
           textSize(20)
         }
 
-        if (state === `translateCHN`) {
+    if (state === `translateCHN`) {
           translateCHN()}
 
           function translateCHN() {
@@ -212,37 +219,37 @@ if (state === `translateENG`) {
             pop();
             text(`爱`, width/2 -20, height/2 - 90);
             textSize(20)
-          }
+        }
 
-          if (state === `translateITA`) {
+      if (state === `translateITA`) {
             translateITA()}
 
-            function translateITA() {
+          function translateITA() {
+            fill(255);
+            push();
+            textSize(90);
+            fill(255)
+            textAlign(CENTER,CENTER);
+            text(`ITALIAN`,width/2, height/2);
+            pop();
+            text(`amore`, width/2 -20, height/2 - 90);
+            textSize(20)
+        }
+
+      if (state === `translateVNM`) {
+            translateVNM()}
+
+            function translateVNM() {
               fill(255);
               push();
               textSize(90);
               fill(255)
               textAlign(CENTER,CENTER);
-              text(`ITALIAN`,width/2, height/2);
+              text(`VIETNAMESE`,width/2, height/2);
               pop();
-              text(`amore`, width/2 -20, height/2 - 90);
+              text(`yêu`, width/2 -20, height/2 - 90);
               textSize(20)
-            }
-
-            if (state === `translateVNM`) {
-              translateVNM()}
-
-              function translateVNM() {
-                fill(255);
-                push();
-                textSize(90);
-                fill(255)
-                textAlign(CENTER,CENTER);
-                text(`VIETNAMESE`,width/2, height/2);
-                pop();
-                text(`yêu`, width/2 -20, height/2 - 90);
-                textSize(20)
-              }
+          }
 
 function display (){
   ellipse(mouseX,mouseY,user.size)
@@ -251,42 +258,44 @@ function display (){
 function setupSimulation(){
   push();
     textSize(35)
-    fill(200,100,100)
+    fill(217, 61, 113)
 
   eng.x = random(60,windowWidth-80);
   eng.y = fr.y-80;
-  text(eng.string,eng.x,eng.y);
+    text(eng.string,eng.x,eng.y);
 
   fr.x = random(60,windowWidth-80);
   fr.y = spn.y-80;
-  text(fr.string,fr.x,fr.y);
+    text(fr.string,fr.x,fr.y);
 
   spn.x = random(60,windowWidth-80);
   spn.y = kr.y-80;
-  text(spn.string,spn.x,spn.y);
+    text(spn.string,spn.x,spn.y);
 
   kr.x = random(60,windowWidth-80);
   kr.y = chn.y-80;
-  text(kr.string,kr.x,kr.y);
+    text(kr.string,kr.x,kr.y);
 
   chn.x = random(60,windowWidth-80);
   chn.y = ita.y - 80;
-  text(chn.string,chn.x,chn.y);
+    text(chn.string,chn.x,chn.y);
 
   ita.x = random(60,windowWidth-80);
   ita.y = vnm.y - 80;
-  text(ita.string,ita.x,ita.y);
+    text(ita.string,ita.x,ita.y);
 
   vnm.x = random(60,windowWidth-80);
   vnm.y = windowHeight-80;
-  text(vnm.string,vnm.x,vnm.y);
+    text(vnm.string,vnm.x,vnm.y);
   pop();
 
+
 push();
-fill(200,100,100);
-  ily.x = windowWidth - 50;
+fill(217, 61, 113);
+  ily.x = windowWidth - 24;
   ily.y = windowHeight;
-  text(ily.string,ily.x,ily.y)
+  textSize(10);
+  text(ily.string,ily.x,ily.y);
   pop();
 }
 
