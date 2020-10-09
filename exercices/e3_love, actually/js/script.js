b3aa6e92c70f5526debd2f3c5fd44526a415293d
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2020-10-09 00:39:27 -0400

    E3: Basic set up

--- exercices/e3_love, actually/js/script.js
+++ exercices/e3_love, actually/js/script.js
@@ -1,150 +1,133 @@
 /**************************************************
-activity p5 looking for love
-Pippin Barr
+Exercise 03: Love, actually
+Vivian Bui
 
-Here is a description of this template p5 project.
+LOVE SIMULATION
 **************************************************/
 
 
-let circleA ={
+let user ={
   x: 100,
   y: 300,
-  size: 100,
-  vx: 0,
-  vy: 0,
-  speed: 3
+  size: 60,
 };
 
-let circleB ={
-  x: 300,
-  y: 200,
-  size: 100,
-  vx: 0,
-  vy: 0,
-  speed: 3
+let eng ={
+  string:`love`,
+    x:0,
+    y:0
 };
+
+let fr ={
+  string:`amour`,
+    x:0,
+    y:0
+};
+
+let spn ={
+  string:`amor`,
+    x:0,
+    y:0
+};
+
+let kr ={
+  string:`사랑`,
+    x:0,
+    y:0
+};
+
+let chn ={
+  string:`爱`,
+    x:0,
+    y:0
+}
+
+let vnm ={
+  string:`yêu`,
+    x:0,
+    y:0
+}
+
 
 let state = `title`
 
-// setup()
-//
-// Description of setup() goes here.
 function setup() {
-createCanvas(500,500);
-setupCircles();
+createCanvas(windowWidth,windowHeight);
 }
 
-function setupCircles() {
-// separate circle positions
-circleA.x = width / 3;
-circleB.x = 2 * width / 3
-
-//random movement
-circleA.vx = random(-circleA.speed,circleA.speed)
-circleB.vx = random(-circleB.speed,circleB.speed)
-circleA.vy = random(-circleA.speed,circleA.speed)
-circleB.vy = random(-circleB.speed,circleB.speed)
-}
-
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
-background(0);
+background(200,100,100);
+noStroke();
 simulation();
-
-if (state === `title`) {
-title();
-}
-
-else if (state === `simulation`){
-  simulation();
-}
-else if (state === `love`){
- love();
-}
-else if (state === `sadness`){
-  sadness();
-}
+noCursor();
 
 function title(){
   push();
   textSize(64);
-  fill(200,100,100);
+  fill(0);
   textAlign(CENTER,CENTER);
   text(`LOVE`,width/2, height/2);
   pop();
 }
 
-function simulation(){
-  move();
-  checkOffscreen();
-  checkOverlap();
-  display();
-}}
+if (state === `title`) {
+title();
+text(`ACROSS LANGUAGES`, 250,200);
+    text(`press to start`, 250,250);
 
-function love() {
+}
+
+else if (state === `simulation`){
+  simulation();
+}
+
+function simulation(){
+  display();
+}
+
+function translateENG() {
   push();
   textSize(64);
   fill(255,150,150);
   textAlign(CENTER,CENTER);
-  text(`ILY`,width/2, height/2);
+  text(`ENGLISH`,width/2, height/2);
   pop();
 }
 
-function sadness() {
+function translateFR() {
   push();
   textSize(64);
-  fill(200,100,100);
+  fill(255,150,150);
   textAlign(CENTER,CENTER);
-  text(`F`,width/2, height/2);
+  text(`FRENCH`,width/2, height/2);
   pop();
 }
 
-function move() {
+function checkTranslation(){
 
-  //circleA movement
-  circleA.x += circleA.vx;
-  circleA.y += circleA.vy;
-  //circleB movement
-  circleB.x += circleB.vx;
-  circleB.y += circleB.vy;
+if (mouseIsPressed && dist(mouseX,mouseY,eng.x,eng.y)<5) {
+  state = translateENG
 }
 
-function checkOffscreen() {
-  //check if the circles have offscreen
-if (circleA.x < 0 || circleA.x > width || circleA.y < 0||circleA.y > height ||
-  circleB.x < 0 || circleB.x > width || circleB.y < 0||circleB.y > height){
-state=`sadness`}}
-
-// if (isOffscreen(circleA) || isOffscreen(circleB); {
-// state = `sadness`;}
-// }
-//
-// function isOffscreen(circle) {
-//   if (circle.x < 0 || circle.x > width || circle.y < 0 || circle.y > height){
-//     return true;
-//   }
-//   else {
-//     return false;
-//   }
-// }
-
-function checkOverlap() {
-  let d = dist(circleA.x,circleA.y,circleB.x,circleB.y);
-  if (d < circleA.size/2 + circleB.size/2){
-    state = `love`
-  }
+if(mouseIsPressed && dist(mouseX,mouseY,fr.x,fr.y)<5) {
+  state = translateFR
 }
+
+if(mouseIsPressed && dist(mouseX,mouseY,spn.x,spn.y)<5) {
+  state = translateSPN
+}
+
+
+}
+
+
 function display (){
-  //sad ending
-
-  ellipse(circleA.x,circleA.y,circleA.size)
-  ellipse(circleB.x,circleB.y,circleB.size)
+  ellipse(mouseX,mouseY,user.size)
 }
 
 function mousePressed(){
   if (state ===`title`) {
     state = `stimulation`
   }
+}
 }
