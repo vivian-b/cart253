1af8de8ffe41913900d0b65d998ce91a58c26503
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2020-10-09 02:04:40 -0400

    E3: Main codes

--- exercices/e3_love, actually/js/script.js
+++ exercices/e3_love, actually/js/script.js
@@ -9,13 +9,13 @@ LOVE SIMULATION
 let user ={
   x: 100,
   y: 300,
-  size: 60,
+  size: 75,
 };
 
 let eng ={
   string:`love`,
     x:0,
-    y:0
+    y:0,
 };
 
 let fr ={
@@ -31,13 +31,19 @@ let spn ={
 };
 
 let kr ={
-  string:`사랑`,
+  string:`연애`,
     x:0,
     y:0
 };
 
 let chn ={
   string:`爱`,
+    x:0,
+    y:0
+}
+
+let ita ={
+  string:`amore`,
     x:0,
     y:0
 }
@@ -70,64 +76,119 @@ function title(){
   pop();
 }
 
+
 if (state === `title`) {
 title();
-text(`ACROSS LANGUAGES`, 250,200);
-    text(`press to start`, 250,250);
+text(`Across Languages`, windowWidth/2 -50,windowHeight/2 +50);
+    text(`press to enter start`, 5,windowHeight-5)}
 
-}
 
-else if (state === `simulation`){
+if (keyIsDown(13)) {
+      state = `stimulation`
+    }
+
+if (state === `simulation`){
   simulation();
+  text(`press backspace to restart`, 5,windowHeight-5)
 }
+
 
 function simulation(){
   display();
+  setupSimulation();
+  checkTranslation()
 }
 
-function translateENG() {
-  push();
-  textSize(64);
-  fill(255,150,150);
-  textAlign(CENTER,CENTER);
-  text(`ENGLISH`,width/2, height/2);
-  pop();
-}
+////
+
 
 function translateFR() {
-  push();
   textSize(64);
-  fill(255,150,150);
   textAlign(CENTER,CENTER);
   text(`FRENCH`,width/2, height/2);
-  pop();
 }
 
 function checkTranslation(){
 
-if (mouseIsPressed && dist(mouseX,mouseY,eng.x,eng.y)<5) {
-  state = translateENG
+  if (mouseIsPressed && dist(mouseX,mouseY,eng.x,eng.y) < 15){
+      state = `translateENG`
 }
 
 if(mouseIsPressed && dist(mouseX,mouseY,fr.x,fr.y)<5) {
-  state = translateFR
+  state = `translateFR`
 }
 
 if(mouseIsPressed && dist(mouseX,mouseY,spn.x,spn.y)<5) {
-  state = translateSPN
+  state = `translateSPN`
 }
 
-
 }
+
+if (state === `translateENG`) {
+  translateENG();
+}
+  function translateENG() {
+    push();
+    textSize(90);
+    fill(255)
+    textAlign(CENTER,CENTER);
+    text(`ENGLISH`,width/2, height/2);
+    pop();
+  }
+
+  if (state === `translateFR`) {
+    translateENG();
+  }
+    function translateFR() {
+      push();
+      textSize(90);
+      fill(255)
+      textAlign(CENTER,CENTER);
+      text(`FRENCH`,width/2, height/2);
+      pop();
+    }
+
+
 
 
 function display (){
   ellipse(mouseX,mouseY,user.size)
 }
 
-function mousePressed(){
-  if (state ===`title`) {
-    state = `stimulation`
-  }
+function setupSimulation(){
+  push();
+    textSize(35)
+    fill(200,100,100)
+
+  eng.x = random(60,windowWidth-80);
+  eng.y = fr.y-80;
+  text(eng.string,eng.x,eng.y);
+
+  fr.x = random(60,windowWidth-80);
+  fr.y = spn.y-80;
+  text(fr.string,fr.x,fr.y);
+
+  spn.x = random(60,windowWidth-80);
+  spn.y = kr.y-80;
+  text(spn.string,spn.x,spn.y);
+
+  kr.x = random(60,windowWidth-80);
+  kr.y = chn.y-80;
+  text(kr.string,kr.x,kr.y);
+
+  chn.x = random(60,windowWidth-80);
+  chn.y = ita.y - 80;
+  text(chn.string,chn.x,chn.y);
+
+  ita.x = random(60,windowWidth-80);
+  ita.y = vnm.y - 80;
+  text(ita.string,ita.x,ita.y);
+
+  vnm.x = random(60,windowWidth-80);
+  vnm.y = windowHeight-80;
+  text(vnm.string,vnm.x,vnm.y);
+  pop();
 }
+
+
 }
