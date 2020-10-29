4263fffbcd40b3308b546a5d31272e368c04afb3
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2020-10-28 21:25:56 -0400

    E5: Creating Classes

---
+++ exercices/e5_juggling-simulation/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Exercise 05: Juggling simulation
+Vivian Bui
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
