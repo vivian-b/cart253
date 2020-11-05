65f716ccbe52c5651742e60fc90ab4845b6bc641
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2020-11-05 17:38:14 -0500

    P2: Player Class

---
+++ projects/project2/js/Player.js
@@ -0,0 +1,63 @@
+class Player {
+
+  constructor(x,y) {
+    this.size = 70;
+
+    this.x = x;
+    this.y = height;
+    this.vx = 0;
+    this.vy = 2;
+    this.speed = 5;
+  }
+
+
+  display() {
+    push();
+    fill(255);
+    noStroke();
+    rectMode(CENTER);
+
+    //body
+    fill(255);
+    ellipse(this.x, this.y, this.size);
+
+    pop();
+
+  }
+
+  move() {
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+
+  this.x = constrain(this.x, 0, width);
+this.y = constrain(this.y, 0 , height);
+
+  }
+
+  handleInput() {
+
+    if (keyIsDown(LEFT_ARROW)) {
+      this.vx = -this.speed * 2;
+    } else if (keyIsDown(RIGHT_ARROW)) {
+      this.vx = this.speed * 2;
+
+    } else {
+      this.vx = 0;
+
+    }
+
+    if (keyIsDown(UP_ARROW)) {
+          this.vy = -this.speed;
+          }
+
+        else if (keyIsDown(DOWN_ARROW)){
+          this.vy = this.speed;
+          }
+
+        else {
+          this.vy = 0;
+          }
+
+}
+
+}
--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -5,16 +5,43 @@ Vivian Bui
 Simulation of a maze with mini-game rooms.
 **************************************************/
 
+
+"use strict";
+
+//Player
+let user;
+
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+createCanvas(750, 750);
+
+
+user = new Player(350, 350);
 
 }
+
+let state = 'title'
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(60,60,60);
 
+  if (state === `title`) {
+   simulation();
+   gameDisplay();
+ }
+
+}
+
+function simulation() {
+  user.move();
+  user.handleInput();
+}
+
+function gameDisplay() {
+  user.display();
 }
