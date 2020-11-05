40a90228a85def7c6ab1c16e5352e6331054e835
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2020-11-05 16:48:38 -0500

    P2: Start of Project

---
+++ projects/project2/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Project 02: Amaze
+Vivian Bui
+
+Simulation of a maze with mini-game rooms.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
