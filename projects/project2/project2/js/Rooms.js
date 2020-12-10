5eedfe315cdbb2ba1e021fb3042c7ab29946c027
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2020-12-09 22:00:47 -0500

    P2: More resizing

--- projects/project2/project2/js/Rooms.js
+++ projects/project2/project2/js/Rooms.js
@@ -8,66 +8,56 @@ class Rooms {
   room1() {
     this.collision();
     background(10, 120, 120);
-    fill(100, 100, 240);
   }
 
   room2() {
     this.collision();
     background(20, 20, 120);
-    fill(100, 100, 240);
   }
+
   room3() {
     this.collision();
     background(30, 20, 120);
-    fill(100, 100, 240);
   }
 
   room4() {
     this.collision();
     background(40, 20, 120);
-    fill(100, 100, 240);
   }
 
   room5() {
     this.collision();
     background(50, 20, 120);
-    fill(100, 100, 240);
   }
 
   room6() {
     this.collision();
     background(60, 20, 120);
-    fill(100, 100, 240);
   }
 
   room7() {
     this.collision();
-    background(70, 20, 120);
-    fill(100, 100, 240);
+    background(45, 145, 84);
   }
 
   room8() {
     this.collision();
     background(80, 20, 120);
-    fill(100, 100, 240);
   }
 
   room9() {
     this.collision();
     background(90, 20, 120);
-    fill(100, 100, 240);
   }
 
   roomEntrance() {
     this.collision();
-    background(30, 0, 20);
-    fill(100, 100, 240);
+    background(45, 145, 84);
   }
 
   roomExit() {
     this.collision();
     background(20, 20, 0);
-    fill(100, 100, 240);
   }
 
   collision() {
